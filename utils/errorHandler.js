3cda425d96159e6c175e227ba4b616f07e0dc372
yizeruier1 <154539188@qq.com>
2019-08-26 18:01:39 +0800

    优化errorHandler中执行回调传参

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -13,8 +13,10 @@ const errorHandler = (res, cb) => {
             myLogin().then(res1 => {
                 if (res1.code === 100) {
                     wx.setStorageSync('token', res1.data)
+                    // 获取回调函数的参数
+                    const param = Array.prototype.slice.call(arguments, 2)
                     // 执行之前需要执行的事件
-                    cb()
+                    cb(...param)
                 } else {
                     tip(res1.msg)
                 }
